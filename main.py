ce6daa538d30b5612c4751c2260049928604e17c
okcul <117544641+okcul@users.noreply.github.com>
2023-08-01 17:32:48 -0400

    token

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from os import listdir
 
-TOKEN = 'MTEzNTIwNTg3ODExNTYxMDczNA.GELkl5.8EQlA7ChJZLmt9HLJjVgKfjyAvpmFdLz73hDFs'
+TOKEN = "TOKEN"
 intents = discord.Intents.default()
 intents.message_content = True
 help_command = commands.DefaultHelpCommand(no_category = 'Misc Commands')
